90c086c9b8fab43ccde01bef0cd83a953c5e8e59
stephanetchatchum <stephanetchatchum@gmail.com>
2026-02-04 13:09:23 +0200

    IF _ ELSE

--- Quiz Game.py
+++ Quiz Game.py
@@ -1,2 +1,14 @@
 print("Hi guys Welcome to the quiz game")
 
+playing = input("Do you want to play? ")
+
+if playing != "yes":
+    quit()
+
+print("Okay! Let's play 😁")
+
+answer = input("What does CPU stand for? ")
+if answer == "central processing unit":
+    print("Correct!✅")
+else: 
+    print("Incorrect!❌")
